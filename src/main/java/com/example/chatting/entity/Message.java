ff4ff9e34277919b64d3958f1402db204e224423
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-04 22:44:06 +0900

    feat: Add Websocket Handler

--- src/main/java/com/example/chatting/entity/Message.java
+++ src/main/java/com/example/chatting/entity/Message.java
@@ -1,4 +1,36 @@
 package com.example.chatting.entity;
 
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
+
+    private String type;
+    private String sender;
+    private String receiver;
+    private Object data;
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void newConnect() {
+        this.type = "new";
+    }
+
+    public void closeConnect() {
+        this.type = "close";
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "type='" + type + '\'' +
+                ", sender='" + sender + '\'' +
+                ", receiver='" + receiver + '\'' +
+                ", data=" + data + '}';
+    }
 }
--- src/main/java/com/example/chatting/handler/WebSocketHandler.java
+++ src/main/java/com/example/chatting/handler/WebSocketHandler.java
@@ -1,4 +1,98 @@
 package com.example.chatting.handler;
 
-public class WebSocketHandler {
+import com.example.chatting.entity.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    public WebSocketHandler(HttpMessageConverters messageConverters) {
+    }
+
+    // 웹소켓 연결
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("[afterConnectionEstablished] " + session.getId());
+        var sessionId = session.getId();
+        sessions.put(sessionId, session);
+
+        Message message = Message.builder()
+                .sender(sessionId)
+                .receiver("all")
+                .build();
+        message.newConnect();
+
+        sessions.values().forEach(s -> {
+            try {
+                if (!s.getId().equals(sessionId)) {
+                    s.sendMessage(new TextMessage(message.toString()));
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Error while sending message to session: " + s.getId(), e);
+            }
+        });
+    }
+
+    // 양방향 데이터 통신
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws Exception{
+        Message message = getObject(textMessage.getPayload());
+        message.setSender(session.getId());
+
+        WebSocketSession receiver = sessions.get(message.getReceiver());
+        if (receiver != null && receiver.isOpen()) {
+            receiver.sendMessage(new TextMessage(message.toString()));
+        }
+    }
+
+    private Message getObject(String textMessage) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Message message = objectMapper.readValue(textMessage, Message.class);
+            return message;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // 소켓 연결 종료
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception{
+        var sessionId = session.getId();
+
+        sessions.remove(sessionId);
+
+        final Message message = new Message();
+        message.closeConnect();
+        message.setSender(sessionId);
+
+        sessions.values().forEach(s -> {
+            try {
+                s.sendMessage(new TextMessage(message.toString()));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    // 소켓 통신 에러
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
+    }
+
 }
